807d697a4e4d55d17056573110bbf8d8b082fc55
fozlerabbi321 <fozlerabbiinfo@gmail.ocm>
2024-01-01 12:10:12 +0600

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,202 @@
+class User {
+  final String username;
+  final String profileImageUrl;
+  final String subscribers;
+
+  const User({
+    required this.username,
+    required this.profileImageUrl,
+    required this.subscribers,
+  });
+}
+
+const User currentUser = User(
+  username: 'F Rabbi',
+  profileImageUrl: 'https://i.postimg.cc/L4Bkvf0L/317655876-1337083793737044-804442137081145709-n-1.jpg',
+  subscribers: '10M',
+);
+
+class Video {
+  final String id;
+  final User author;
+  final String title;
+  final String thumbnailUrl;
+  final String duration;
+  final DateTime timestamp;
+  final String viewCount;
+  final String likes;
+  final String dislikes;
+
+  const Video({
+    required this.id,
+    required this.author,
+    required this.title,
+    required this.thumbnailUrl,
+    required this.duration,
+    required this.timestamp,
+    required this.viewCount,
+    required this.likes,
+    required this.dislikes,
+  });
+}
+
+final List<Video> videos = [
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+
+];
+
+final List<Video> suggestedVideos = [
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'রমজানের আগে একবার শুনুন l নিজেকে পরিবর্তন করে নিন l আবু ত্বহা আদনান h',
+    thumbnailUrl: 'https://wallpaperaccess.com/full/380479.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10M',
+    likes: '958',
+    dislikes: '4',
+  ),
+];
